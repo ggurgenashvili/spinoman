ac009bc5abadd57b9934132c2800d7a8b28db8f9
ggurgenashvili <ggurgenashvili@spinoman.com>
2024-09-23 18:11:32 +0400

    blog page //

---
+++ cypress/e2e/blog.spec.js
@@ -0,0 +1,15 @@
+describe('blog page', () => {
+    it('see blog. write comment and like blog', () => {
+        cy.visitSpinoman()
+        cy.get('p').contains('Blog').click()
+        cy.wait(4000)
+        cy.get('.blog').find('button').contains('MORE DETAILS ').click()
+
+
+        //write a comment on a blog
+        cy.get('[placeholder="Write your opinion"]').type('{enter}good game')
+
+        //like blog
+        cy.get('.container__like').find('img').eq(0).click()
+    })
+})
